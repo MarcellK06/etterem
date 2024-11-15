efa5c1dfd80cf6cc73c13bf5e66d8e51cf49f4ff
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-11-15 06:45:48 +0000

    asg

---
+++ etterem/Program.cs
@@ -0,0 +1,18 @@
+﻿enum kategoria {
+    Leves,
+    Foetel,
+    Desszert
+}
+
+class Etlap_Elem {
+    public string nev;
+    public float szenhidrat;
+    public int ar;
+    public kategoria etelkategoria;
+}
+
+class Forgalom_Elem {
+    public string nev;
+    public int adag;
+    public DateTime datum;
+}
